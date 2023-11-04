f69ee36468b13e94aa9ea48010e19212cc8e83a6
Naif al-Sahabi <2210002362@iau.edu.sa>
2023-11-04 04:26:14 +0300

    ver1.1

--- Email_generator/main.py
+++ Email_generator/main.py
@@ -2,6 +2,11 @@ import streamlit as st
 import openai
 import os
 from dotenv import load_dotenv
+from PIL import Image
+import time
+import matplotlib.pyplot as plt
+
+
 load_dotenv()
 
 
@@ -58,7 +63,7 @@ def display_notice():
     st.text("Please ensure you don't input sensitive data.")
 
 
-def generate_prompt(email_tone, email_length, email_type, email_Language, subject, description, restrictions, Purpose=None, Sender_Email=None):
+def generate_prompt(email_tone, email_length, email_type, email_Language, subject, description, restrictions, Purpose=None, Sender_Email=None, salutation=None):
     prompt_base = f"""
     you are an expert system in the field of writing emails.
     your will be provided with some specific information about the email that you need to write.
@@ -66,6 +71,7 @@ def generate_prompt(email_tone, email_length, email_type, email_Language, subjec
     please make sure to include all the information provided in the email.
 
     ```
+    salutation: {salutation}
     subject: {subject}
     description: {description}
     email tone: {email_tone}
@@ -80,7 +86,98 @@ def generate_prompt(email_tone, email_length, email_type, email_Language, subjec
         return prompt_base + f"sender email: {Sender_Email}\n```"
 
 
+def sidebar_features():
+    st.sidebar.title("Email Generator Sidebar")
+
+    # ? 1. About Section
+    st.sidebar.header("About")
+    st.sidebar.text(
+        "This app is built using OpenAI's ")
+    st.sidebar.text(
+        "to generate emails based on user input.")
+
+    # ? 2. Feedback Form
+    telegram_icon = "https://cdn3.iconfinder.com/data/icons/social-icons-33/512/Telegram-16.png"
+    st.sidebar.header("Feedback")
+    st.sidebar.subheader("Provide Feedback on my Telegram:")
+    feedback_url = "https://t.me/Naifcx4735"
+
+    html_code = f"""
+    <ul style="list-style-type: none; padding-left: 0;">
+        <li><a href="{feedback_url}" target="_blank"><img src="{telegram_icon}" width="20" height="20"/>  Feedback</a></li>
+    """
+
+    st.sidebar.markdown(html_code, unsafe_allow_html=True)
+
+    # ? 3. Social Media
+    st.sidebar.header("Follow me on:")
+
+    # Replace with your actual icon URL
+    github_icon = "https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Github-16.png"
+    twitter_icon = "https://cdn4.iconfinder.com/data/icons/social-media-black-white-2/1227/X-16.png"
+    linkedin_icon = "https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Linkedin_unofficial_colored_svg-16.png"
+
+    github_link = "https://github.com/Naifcx47350"
+    twitter_link = "https://twitter.com/Naif4735"
+    linkedin_link = "https://www.linkedin.com/in/naif-alsahabi-085720249/"
+
+    html_code = f"""
+    <ul style="list-style-type: none; padding-left: 0;">
+        <li><a href="{github_link}" target="_blank"><img src="{github_icon}" width="20" height="20"/> GitHub</a></li>
+        <li><a href="{twitter_link}" target="_blank"><img src="{twitter_icon}" width="20" height="20"/> X(Twitter)</a></li>
+        <li><a href="{linkedin_link}" target="_blank"><img src="{linkedin_icon}" width="20" height="20"/> LinkedIn</a></li>
+    </ul>
+    """
+
+    st.sidebar.markdown(html_code, unsafe_allow_html=True)
+
+    # ? 4. Usage Tips
+    st.sidebar.header("Usage Tips")
+    st.sidebar.markdown(
+        """
+            - Don't input sensitive data.
+            - Provide as much information as possible.
+            - Provide a clear description , subject and purpose.
+            - The restrictions can be words to include or exclude, a specific format, or any other restrictions you would like to put on the email.
+            - You can view the previously generated emails by clicking on the expander below the generate email button.
+        """
+    )
+
+
+def show_progress_bar():
+    """Show progress bar while generating email."""
+    progress_bar = st.progress(0)
+    for i in range(50, 101, 10):
+        time.sleep(0.2)
+        progress_bar.progress(i)
+    time.sleep(1)
+    progress_bar.empty()
+    return progress_bar
+
+
+def choose_salutation():
+    # ? Selecting the salutation category
+    salutation_category = st.radio(
+        'Select the type of salutation:',
+        ('General', 'Formal', 'Friendly', 'Time-based', 'Specific')
+    )
+
+    # ? Defining the salutations
+    salutations = {
+        'General': ['Hello', 'Hi', 'Greetings', 'To whom it may concern', 'Hey'],
+        'Formal': ['Dear [Name]', 'Dear Sir/Madam', 'Dear Dr. [Name]', 'Attention: [Department/Title]'],
+        'Friendly': ['Hey there', 'Hiya', 'Howdy'],
+        'Time-based': ['Good morning', 'Good afternoon', 'Good evening'],
+        'Specific': ['Dear Team', 'Dear valued customer', 'Dear [Company Name]', 'Dear Hiring Manager', 'Dear HR Department']
+    }
+
+    chosen_salutation = st.selectbox(
+        'Choose a salutation:', salutations[salutation_category])
+    return chosen_salutation
+
+
 def main():
+    sidebar_features()
     st.title('Email Generator')
     st.subheader(
         "welcome to the email generator app, this app will help you generate an email based on the information you provide.")
@@ -110,6 +207,10 @@ def main():
         'What type of email would you like to generate?',
         ('English', 'Arabic', 'Spanish')
     )
+
+    st.markdown('---')
+
+    salutation = choose_salutation()
 
     st.markdown('---')
 
@@ -144,10 +245,18 @@ def main():
 
     # ? Generate the email
     if st.button('Generate Email'):
+
+        if not subject or not description:
+            st.warning(
+                "Please provide both a subject and a description for the email.")
+            return
+
         prompt = generate_prompt(email_tone, email_length, email_type,
-                                 email_Language, subject, description, restrictions, Purpose)
+                                 email_Language, subject, description, restrictions, Purpose, salutation)
         try:
+
             email_content = generate_email(prompt, email_tone, email_length)
+            progress_bar = show_progress_bar()
             st.session_state.generated_emails.append(email_content)
 
             st.subheader('Generated Email:')
@@ -157,6 +266,7 @@ def main():
                                file_name='generated_email.txt')
 
         except Exception as e:
+            progress_bar.empty()
             st.error(
                 'Something went wrong, please try again or contact the developer.')
             st.error(e)
@@ -169,6 +279,9 @@ def main():
                              height=400, key=idx)
                 st.download_button(
                     f'Download Email {idx}', email, file_name=f'generated_email_{idx}.txt')
+
+    st.caption('Made by Naif Alsahabi')
+    st.caption('v1.0')
 
 
 if __name__ == '__main__':
