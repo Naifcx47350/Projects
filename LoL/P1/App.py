6c6cc0a54ac2f0d159c79083089f16196bd3e1b5
Naif al-Sahabi <2210002362@iau.edu.sa>
2024-04-17 13:58:23 +0300

    Testing

--- LoL/P1/App.py
+++ LoL/P1/App.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+DB = SQLAlchemy()
+DB_NAME = 'database.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+DB.init_app(app)
+
+
+class Todo(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    content = DB.Column(DB.String(200), nullable=False)
+    data_created = DB.Column(DB.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 
 @app.route('/')
